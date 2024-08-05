4d582031b58adba062ad665cf928815326d7e389
wei10cool <x215033@yahoo.com.tw>
2024-08-05 17:04:45 +0800

    git init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+//外部套件
+import {useEffect, useState} from 'react'; 
+// import axios from 'axios';
+
+//專案內套件
+import logo from './assets/logo.svg';
+import './assets/App.css';
+import Input from './components/Input';
+import './assets/all.scss';
 
 function App() {
+  const [text,setText] = useState('');
+  const onChangeHandler = (e)=>{
+    setText(e.target.value);
+  }
+
+  useEffect(()=>{
+    (async()=>{
+      console.log("build:",process.env.REACT_APP_PATH);
+      console.log("all:",process.env.REACT_APP_all);
+      // const result =await axios.get(path);
+      // console.log(result);
+    })();//立即執行
+  },[])//[]載入時，只執行一次
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br/>
+          <button type="button" className="btn btn-primary">Primary</button>
+          {text}
+          <Input id="sampleText" text="hello" value={text} onChangeHandler={onChangeHandler} />
         </p>
         <a
           className="App-link"
